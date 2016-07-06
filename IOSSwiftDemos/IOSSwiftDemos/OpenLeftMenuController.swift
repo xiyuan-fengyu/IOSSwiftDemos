2f553836b9c4dc75f6e1453dc7187040ff099bf4
xiyuan-fengyu <xiyuan-fengyu@163.com>
2016-07-06 21:57:22 +0800

    LeftMenuController实现

--- IOSSwiftDemos/IOSSwiftDemos/OpenLeftMenuController.swift
+++ IOSSwiftDemos/IOSSwiftDemos/OpenLeftMenuController.swift
@@ -1,25 +1,36 @@
 //
-//  LeftMenuController.swift
+//  OpenLeftMenuController.swift
 //  IOSSwiftDemos
 //
-//  Created by xiyuan_fengyu on 16/7/6.
+//  Created by xiyuan_fengyu on 16/7/5.
 //  Copyright © 2016年 xiyuan. All rights reserved.
 //
 
 import UIKit
 
-class LeftMenuController: UIViewController {
+class OpenLeftMenuController: UIViewController {
+
+    private var leftMenuController: LeftMenuController!
     
-    private var backImg: UIImageView!
+    @IBAction func openMenuClick(sender: AnyObject) {
+        self.view.window?.addSubview(leftMenuController.view)
+        leftMenuController.view.frame.origin.x = 100
+        leftMenuController.view.frame.origin.y = 100
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        backImg = UIImageView(frame: self.view.frame)
-        backImg.image = UIImage(named: "bg2")
-        self.view.addSubview(backImg)
+        leftMenuController = LeftMenuController()
+        // Do any additional setup after loading the view.
     }
 
+    override func viewWillDisappear(animated: Bool) {
+        self.leftMenuController.view.removeFromSuperview()
+        self.leftMenuController = nil
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
